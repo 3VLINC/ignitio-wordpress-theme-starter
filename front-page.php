8a44a07d0e0332cc0b16322effadf8050a8c301f
Stephanie Carter <stephanie@3vl.ca>
2015-02-19 16:45:39 -0500

    Reverting front-page.php (because I was just testing the menu there)

--- front-page.php
+++ front-page.php
@@ -1,11 +1,6 @@
-<?php namespace Theme ?>
-
 <?php get_template_part('template-parts/page--header'); ?>
 
 <div class="template--front-page">	
-
-
-	<?php Menus::outputMenu(Menus::MN_SOCIAL_MEDIA); ?>
 
 </div>
 		
